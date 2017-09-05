d879ec4ac3c9cd3d12edfbfd9da6bb25fbf5ac1c
Anderson Silva <anderson@edu.univali.br>
2017-09-04 22:05:47 -0300

    alterações

--- TrabalhoM1/TrabalhoM1/TrabalhoM1.cpp
+++ TrabalhoM1/TrabalhoM1/TrabalhoM1.cpp
@@ -1,11 +1,13 @@
 // TrabalhoM1.cpp : Defines the entry point for the console application.
 //
-
+#include "interface_usuario.h"
 #include "stdafx.h"
 #include <iostream>
 
 int main()
-{
+{   
+
+	imprimeMatriz(char matriz[TAMANHO_MATRIZ][TAMANHO_MATRIZ]);
     return 0;
 }
 
--- TrabalhoM1/TrabalhoM1/interface_usuario.h
+++ TrabalhoM1/TrabalhoM1/interface_usuario.h
@@ -4,6 +4,38 @@
 #define INTERFACE_USUARIO_H
 #include<iostream>
 using namespace std;
+#define TAMANHO_MATRIZ 3;
+
+void imprimeMatriz(char matriz[][]) {
+
+	int i, j;
+
+	for (i = 0; i < TAMANHO_MATRIZ; i++) {
+		for (i = 0; i < TAMANHO_MATRIZ; i++) {
+
+			matriz[i][j] = " ";
+		}
+
+
+	}
+
+	for (i = 0; i < TAMANHO_MATRIZ; i++) {
+		for (i = 0; i < TAMANHO_MATRIZ; i++) {
+
+			cout << " | " << matriz[i][j]endl;
+		}
+
+
+	}
+
+
+}
+
+
+
+
+
+
 
 
 #endif
